b8954af5c6e4bad4b9adc114219746531f58d1b2
StlMaris123 <stellamarisnjage@gmail.com>
2020-01-14 13:08:28 +0200

    custom styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <di>Hi there!</di>
+    return (
+        <div>
+            <lable className="label" for="name">
+                Enter Name:
+            </lable>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        </div>
+    )
 };
 
 ReactDOM.render(
