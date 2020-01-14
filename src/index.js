f81a4d6ac8052fe03e05986be4b844f2fc914009
StlMaris123 <stellamarisnjage@gmail.com>
2020-01-14 13:14:19 +0200

    referencing variables

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    const buttonText = 'Click Me'
     return (
         <div>
             <lable className="label" for="name">
                 Enter Name:
             </lable>
             <input id="name" type="text" />
-            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button>
         </div>
     )
 };
